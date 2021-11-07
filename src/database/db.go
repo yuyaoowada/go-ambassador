6cd2d054b8c13e6643200d141b8f750b846c5d24
yuyaoowada <yuyaoowada@gmail.com>
2021-11-07 16:09:20 +0900

    create order-function

--- src/database/db.go
+++ src/database/db.go
@@ -19,5 +19,5 @@ func Connect() {
 }
 
 func AutoMigrate() {
-	DB.AutoMigrate(models.User{}, models.Product{}, models.Link{})
+	DB.AutoMigrate(models.User{}, models.Product{}, models.Link{}, models.Order{}, models.OrderItems{})
 }
